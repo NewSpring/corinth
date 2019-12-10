36a3c6fe44c66b546e28dfad36a0c6879615e107
Michael Neeley <micneeley14@gmail.com>
2019-12-10 15:46:26 -0500

    style: Changed Android Icons

--- scripts/generate-icons.js
+++ scripts/generate-icons.js
@@ -1,8 +1,19 @@
 const fs = require('fs');
 const Jimp = require('jimp');
+const circle = require('@jimp/plugin-circle');
+const configure = require('@jimp/custom');
 const sizeOf = require('image-size');
 
-const iconDirs = ['ios/newspringchurchapp/Images.xcassets/AppIcon.appiconset/'];
+configure({ plugins: [circle] }, Jimp);
+
+const iconDirs = [
+  'ios/newspringchurchapp/Images.xcassets/AppIcon.appiconset/',
+  'android/app/src/main/res/mipmap-mdpi/',
+  'android/app/src/main/res/mipmap-hdpi/',
+  'android/app/src/main/res/mipmap-xhdpi/',
+  'android/app/src/main/res/mipmap-xxhdpi/',
+  'android/app/src/main/res/mipmap-xxxhdpi/',
+];
 
 const [template] = process.argv.slice(2);
 if (!template) {
@@ -12,15 +23,20 @@ if (!template) {
 
 iconDirs.forEach((dir) => {
   fs.readdir(dir, (e, files) => {
-    if (e) throw e;
     files.forEach((file) => {
-      if (!file.match(/^acicon/)) return;
+      // define valid file prefixes
+      if (!file.match(/^acicon/) && !file.match(/^ic/)) return;
       const path = dir + file;
       sizeOf(path, (e1, { width, height }) => {
-        if (e1) throw e1;
         Jimp.read(template, (e2, icon) => {
-          if (e2) throw e2;
-          icon.resize(width, height).write(path);
+          if (file.match(/.*round.*/)) {
+            icon
+              .circle()
+              .resize(width, height)
+              .write(path);
+          } else {
+            icon.resize(width, height).write(path);
+          }
         });
       });
     });
