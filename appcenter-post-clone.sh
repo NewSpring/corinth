25102d65a8b1d57e225dc26f2c34d85b6b8dc365
Michael Neeley <micneeley14@gmail.com>
2020-01-02 09:21:44 -0500

    removed pre build script

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -16,3 +16,5 @@ COCOAPODS_VER=`sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' ios/Podfile.lock`
 
 echo "Installing CocoaPods version $COCOAPODS_VER"
 sudo gem install cocoapods -v $COCOAPODS_VER
+cd ios
+pod install --repo-update
