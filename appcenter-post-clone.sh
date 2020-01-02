c2c57462c841b3e4bfb57d05566df7b1093f098a
Michael Neeley <micneeley14@gmail.com>
2020-01-02 10:00:56 -0500

    script improvements

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -12,9 +12,8 @@ cp .env.production .env
 echo "Uninstalling all CocoaPods versions"
 sudo gem uninstall cocoapods --all --executables
 
-COCOAPODS_VER=`sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' ios/Podfile.lock`
+COCOAPODS_VER=$(sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' ios/Podfile.lock)
 
 echo "Installing CocoaPods version $COCOAPODS_VER"
-sudo gem install cocoapods -v $COCOAPODS_VER
-cd ios
-pod install --repo-update
+sudo gem install cocoapods -v "$COCOAPODS_VER"
+yarn pods
--- scripts/bump-version-code.sh
+++ scripts/bump-version-code.sh
@@ -4,8 +4,9 @@
 # test track - sets current date and time
 # production track - increments by one (has to be less than test track)
 VERSION_CODE=$(sed -n -e 's/versionCode \([0-9]+\)/\1/p' android/app/build.gradle)
-if [ "$BUGSNAG_STAGE" != $"production" ]; then
-	sed -i "" -E "s/versionCode [0-9]+/versionCode $(date -u +"%y%m%d%H%M")/g" android/app/build.gradle
-else
+echo $VERSION_CODE
+if [ "$BUGSNAG_STAGE" = "production" ]; then
 	sed -i "" -E "s/versionCode [0-9]+/versionCode $((VERSION_CODE + 1))/g" android/app/build.gradle
+else
+	sed -i "" -E "s/versionCode [0-9]+/versionCode $(date -u +"%y%m%d%H%M")/g" android/app/build.gradle
 fi
