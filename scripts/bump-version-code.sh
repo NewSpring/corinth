3fa19540683a8360c0d3f4f772021e13a1f84233
Michael Neeley <micneeley14@gmail.com>
2020-01-02 10:24:32 -0500

    fixes the script one last time

--- scripts/bump-version-code.sh
+++ scripts/bump-version-code.sh
@@ -3,8 +3,7 @@
 # overwrites android version code
 # test track - sets current date and time
 # production track - increments by one (has to be less than test track)
-VERSION_CODE=$(sed -n -e 's/versionCode \([0-9]+\)/\1/p' android/app/build.gradle)
-echo $VERSION_CODE
+VERSION_CODE=$(sed -n -e '/versionCode/s/.* //p' android/app/build.gradle | sed -n 1p)
 if [ "$BUGSNAG_STAGE" = "production" ]; then
 	sed -i "" -E "s/versionCode [0-9]+/versionCode $((VERSION_CODE + 1))/g" android/app/build.gradle
 else
