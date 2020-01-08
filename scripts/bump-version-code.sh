ccfbf851f812314d26f80cdaaa9a76e0a15e7f8b
Michael Neeley <micneeley14@gmail.com>
2020-01-08 12:35:23 -0500

    chore: Sets Production Verson Code (#16)

--- scripts/bump-version-code.sh
+++ scripts/bump-version-code.sh
@@ -5,7 +5,7 @@
 # production track - increments by one (has to be less than test track)
 VERSION_CODE=$(sed -n -e '/versionCode/s/.* //p' android/app/build.gradle | sed -n 1p)
 if [ "$BUGSNAG_STAGE" = "production" ]; then
-	sed -i "" -E "s/versionCode [0-9]+/versionCode $((VERSION_CODE + 1))/g" android/app/build.gradle
+	sed -i "" -E "s/versionCode [0-9]+/versionCode $((VERSION_CODE - 1))/g" android/app/build.gradle
 else
 	sed -i "" -E "s/versionCode [0-9]+/versionCode $(date -u +"%y%m%d%H%M")/g" android/app/build.gradle
 fi
