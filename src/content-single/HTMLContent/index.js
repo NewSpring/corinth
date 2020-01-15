9a68c1e4c2e577a8c99c8facfe28d1d3e630abf2
Michael Neeley <micneeley14@gmail.com>
2020-01-15 12:01:36 -0500

    chore: Notifies for Bad Content Images

--- src/content-single/HTMLContent/index.js
+++ src/content-single/HTMLContent/index.js
@@ -3,14 +3,36 @@ import PropTypes from 'prop-types';
 import { Query } from 'react-apollo';
 import InAppBrowser from 'react-native-inappbrowser-reborn';
 
-import HTMLView from '@apollosproject/ui-htmlview';
+import HTMLView, { defaultRenderer } from '@apollosproject/ui-htmlview';
 import { ErrorCard } from '@apollosproject/ui-kit';
 
+import bugsnag from '../../bugsnag';
 import GET_CONTENT_ITEM_CONTENT from './getContentItemContent';
 
 function handlePressAnchor(url) {
   return InAppBrowser.open(url);
 }
+
+const isLocalImg = (src) => src.startsWith('/');
+
+const customRenderer = (contentId) => (node, ...otherArgs) => {
+  if (node.name === 'img' && isLocalImg(node.attribs.src)) {
+    bugsnag.notify(new Error(`Bad image URL`), (report) => {
+      // eslint-disable-next-line
+      report.metaData = {
+        metaData: { url: node.attribs.src, contentId },
+        severity: 'warning',
+      };
+    });
+    return null;
+  }
+  return defaultRenderer(node, ...otherArgs);
+};
+const CustomHTMLView = (props) => (
+  <HTMLView renderer={customRenderer(props.contentId)} {...props} />
+);
+
+CustomHTMLView.propTypes = { contentId: PropTypes.string };
 
 const HTMLContent = ({ contentId }) => {
   if (!contentId) return <HTMLView isLoading />;
@@ -22,18 +44,19 @@ const HTMLContent = ({ contentId }) => {
       fetchPolicy={'cache-and-network'}
     >
       {({
-        data: { node: { htmlContent, summary } = {} } = {},
+        data: { node: { htmlContent, summary, id } = {} } = {},
         loading,
         error,
       }) => {
         if (!htmlContent && error) return <ErrorCard error={error} />;
         return (
-          <HTMLView
+          <CustomHTMLView
+            contentId={id}
             isLoading={!htmlContent && !summary && loading}
             onPressAnchor={handlePressAnchor}
           >
             {htmlContent || summary}
-          </HTMLView>
+          </CustomHTMLView>
         );
       }}
     </Query>
