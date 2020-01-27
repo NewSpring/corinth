f0971c1be305ddc931fe97fe67363aeae3674a39
Michael Neeley <micneeley14@gmail.com>
2020-01-27 13:39:52 -0500

    removed version

--- src/bugsnag.js
+++ src/bugsnag.js
@@ -1,13 +1,11 @@
 import { Client, Configuration } from 'bugsnag-react-native';
 import Config from 'react-native-config';
 import { onError } from 'apollo-link-error';
-import { getVersion } from 'react-native-device-info';
 import AsyncStorage from '@react-native-community/async-storage';
 
 const configuration = new Configuration();
 configuration.apiKey = Config.BUGSNAG_API_KEY;
 configuration.releaseStage = Config.BUGSNAG_STAGE || 'development';
-configuration.appVersion = getVersion();
 const bugsnag = new Client(configuration);
 
 const getStore = async () => {
