502d9ff112d3142a9ceba85f0186d5feaa4d2118
Michael Neeley <micneeley14@gmail.com>
2020-01-28 09:24:54 -0500

    fix: Adds a navigation queue

--- src/NavigationService.js
+++ src/NavigationService.js
@@ -1,31 +1,62 @@
-import { NavigationActions } from 'react-navigation';
+import { StackActions, NavigationActions } from 'react-navigation';
 
 let _navigator;
+let _pendingActions = [];
+
+const performWhenReady = (func) => (...args) => {
+  if (_navigator) {
+    func(...args);
+  } else {
+    _pendingActions.push(() => func(...args));
+  }
+};
 
 const setTopLevelNavigator = (navigatorRef) => {
   _navigator = navigatorRef;
+  if (_pendingActions.length > 0) {
+    _pendingActions.forEach((action) => action());
+  }
+  _pendingActions = [];
 };
 
-const navigate = (routeName, params) => {
+const navigate = performWhenReady((routeName, params) => {
   _navigator.dispatch(
     NavigationActions.navigate({
       routeName,
       params,
     })
   );
-};
+});
 
-const goBack = (from) => {
+const resetToAuth = performWhenReady(() => {
+  _navigator.dispatch(
+    StackActions.reset({
+      index: 0,
+      key: null,
+      actions: [
+        NavigationActions.navigate({
+          routeName: 'Auth',
+          action: NavigationActions.navigate({
+            routeName: 'AuthSMSPhoneEntryConnected',
+          }),
+        }),
+      ],
+    })
+  );
+});
+
+const goBack = performWhenReady((from) => {
   let key;
   if (from) {
     const route = _navigator.state.nav.routes.find((r) => r.routeName === from);
     if (route) ({ key } = route);
   }
   _navigator.dispatch(NavigationActions.back({ key }));
-};
+});
 
 export default {
   setTopLevelNavigator,
   navigate,
   goBack,
+  resetToAuth,
 };
