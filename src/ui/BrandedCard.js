97d6f4b4b8d4ddcc5ddb77f889d2703fa2735986
richarddubay <richardtdubayjr@gmail.com>
2020-01-28 13:22:24 -0500

    Added comments on how to put things back to what they were should we need them.

--- src/ui/BrandedCard.js
+++ src/ui/BrandedCard.js
@@ -4,6 +4,38 @@ import { get } from 'lodash';
 
 import { FeaturedCard, CardLabel, styled } from '@apollosproject/ui-kit';
 
+// If we decide to go back to what we had before:
+// * Uncomment the LiveAwareLabel code below
+// * Remove hasAction & campaign from props and propTypes
+// * Remove the StyledCardLabel & getTheme functions
+// * Change LabelComponent to be:
+// LabelComponent={
+//    labelText ? (
+//      <LiveAwareLabel
+//        type={get(theme, 'type') === 'LIGHT' ? 'darkOverlay' : undefined}
+//        customTheme={theme}
+//        isLive={isLive}
+//        title={labelText}
+//      />
+//    ) : null
+
+// const LiveAwareLabel = withTheme(	const StyledCardLabel = styled(({ theme }) => ({
+//   ({ customTheme, isLive, title, type, theme }) => ({	  marginBottom: theme.sizing.baseUnit,
+//     ...(isLive	}))(CardLabel);
+//       ? {
+//           title: 'Live',
+//           type: 'secondary',
+//           icon: 'live-dot',
+//           iconSize: theme.helpers.rem(0.4375), // using our typographic size unit based on fontSize so that the icon scales correctly with font size changes.
+//         }
+//       : {
+//           title,
+//           type: !customTheme ? 'secondary' : type,
+//         }),
+//     style: { marginBottom: theme.sizing.baseUnit },
+//   })
+// )(CardLabel);
+
 const StyledCardLabel = styled(({ theme }) => ({
   marginBottom: theme.sizing.baseUnit,
 }))(CardLabel);
