f506a1c6faffbee871c7bc9262efdeb27630e2de
Michael Neeley <micneeley14@gmail.com>
2020-01-28 22:30:38 -0500

    fixed some tests

--- jest.setup.js
+++ jest.setup.js
@@ -6,14 +6,11 @@ import FRAGMENTS from '@apollosproject/ui-fragments';
 ApollosConfig.loadJs({ FRAGMENTS });
 
 // We ran into an issue where SafeAreaView would break jest tests.
-jest.mock(
-  '../apollos-ui-kit/node_modules/react-native-safe-area-context/',
-  () => ({
-    SafeAreaConsumer: ({ children }) =>
-      children({ top: 0, bottom: 0, left: 0, right: 0 }),
-    SafeAreaProvider: ({ children }) => children,
-  })
-);
+jest.mock('react-native-safe-area-context', () => ({
+  SafeAreaConsumer: ({ children }) =>
+    children({ top: 0, bottom: 0, left: 0, right: 0 }),
+  SafeAreaProvider: ({ children }) => children,
+}));
 
 jest.mock('react-navigation', () => {
   const ActualNavigation = require.requireActual('react-navigation');
@@ -34,7 +31,7 @@ jest.mock('react-native-music-control', () => ({
 
 ApollosConfig.loadJs({
   ONE_SIGNAL_KEY: 'doesntmatter',
-}));
+});
 
 jest.mock('Animated', () => {
   const ActualAnimated = require.requireActual('Animated');
@@ -43,14 +40,14 @@ jest.mock('Animated', () => {
     timing: (value, config) => ({
       start: (callback) => {
         value.setValue(config.toValue);
-        callback && callback();
+        if (callback) callback();
       },
       stop: () => ({}),
     }),
     spring: (value, config) => ({
       start: (callback) => {
         value.setValue(config.toValue);
-        callback && callback();
+        if (callback) callback();
       },
       stop: () => ({}),
     }),
@@ -71,10 +68,6 @@ jest.mock('react-native-device-info', () => ({
 }));
 
 jest.mock('rn-fetch-blob', () => 'Fetch');
-jest.mock(
-  '@apollosproject/ui-passes/node_modules/rn-fetch-blob',
-  () => 'Fetch'
-);
 
 jest.mock('@apollosproject/ui-analytics', () => ({
   track: () => '',
@@ -100,7 +93,7 @@ jest.mock('@apollosproject/ui-media-player', () => ({
 
 jest.mock('react-native-amplitude-analytics', () => {
   class AnalyticsMock {
-    logEvent() {
+    static logEvent() {
       return null;
     }
   }
