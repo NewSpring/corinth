c37813d88df63d767a86122cc12c66dfbc2a951d
Michael Neeley <micneeley14@gmail.com>
2020-01-28 21:07:08 -0500

    adds polyfill file

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ import './polyfill'; // this can be removed when we upgrade to react-native 59
 import './loadConfig';
 import { AppRegistry, YellowBox } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
-import ApollosConfig from '@apollosproject/config';
-import Storybook from './storybook';
+// import ApollosConfig from '@apollosproject/config';
+// import Storybook from './storybook';
 
-const useStorybook = ApollosConfig.STORYBOOK === 'true';
+// const useStorybook = ApollosConfig.STORYBOOK === 'true';
 
-const MainApp = require('./src').default;
+const App = require('./src').default;
 
-let App = MainApp;
-if (useStorybook) {
-  App = Storybook;
-}
+// we don't use storybook, this is from core
+// let App = MainApp;
+// if (useStorybook) {
+// App = Storybook;
+// }
 
 // If there's an error before the splash screen goes away, you never see that error.
 // This tries to hide the splash screen so you can see the error.
