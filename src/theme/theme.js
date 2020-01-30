d06379ab33f03d50c275ee9a4ce3af90f48129f5
richarddubay <richardtdubayjr@gmail.com>
2020-01-30 10:28:57 -0500

    Updated theme to include 'ui' font-family and updated tests

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -89,6 +89,9 @@ const typography = {
       italic: 'Colfax-BlackItalic',
     },
   },
+  ui: {
+    regular: 'Colfax-Regular',
+  },
 };
 
 export const buttons = ({ colors: themeColors, alpha: themeAlpha }) => ({
