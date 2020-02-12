b9a1d49c3f6df0d91c11f2cbefc7386a84f5751d
richarddubay <richardtdubayjr@gmail.com>
2020-02-12 11:16:19 -0500

    Added pageInfo so paging can get the right cursor

--- src/prayer/data/queries/getPrayerFeed.js
+++ src/prayer/data/queries/getPrayerFeed.js
@@ -3,6 +3,9 @@ import gql from 'graphql-tag';
 export default gql`
   query PrayerFeed($type: PrayerType, $first: Int, $after: String) {
     prayerFeed(type: $type, first: $first, after: $after) {
+      pageInfo {
+        endCursor
+      }
       edges {
         cursor
         node {
