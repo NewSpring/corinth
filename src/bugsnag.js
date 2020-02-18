1a7c210f9750795f171ca8b4d57e443662c78f6f
Michael Neeley <micneeley14@gmail.com>
2020-02-18 12:21:24 -0500

    chore: Attaches User to Bugsnag

--- src/bugsnag.js
+++ src/bugsnag.js
@@ -1,42 +1,52 @@
 import { Client, Configuration } from 'bugsnag-react-native';
+import gql from 'graphql-tag';
 import Config from 'react-native-config';
 import { onError } from 'apollo-link-error';
-import AsyncStorage from '@react-native-community/async-storage';
+import { client } from './client';
 
 const configuration = new Configuration();
 configuration.apiKey = Config.BUGSNAG_API_KEY;
 configuration.releaseStage = Config.BUGSNAG_STAGE || 'development';
 const bugsnag = new Client(configuration);
 
-const getStore = async () => {
-  const keys = await AsyncStorage.getAllKeys();
-  return AsyncStorage.multiGet(keys);
-};
-
-const bugsnagLink = onError(({ graphQLErrors, networkError, operation }) => {
-  if (graphQLErrors) {
-    graphQLErrors.map(async ({ message, locations, path }) => {
-      const store = await getStore();
-      bugsnag.notify(new Error(message), (report) => {
-        if (operation.variables && operation.variables.password) {
-          // eslint-disable-next-line
-          delete operation.variables.password;
-        }
-        if (path) {
-          // eslint-disable-next-line
-          report.context = path.join('/');
-        }
-        // eslint-disable-next-line
-        report.metadata = {
-          path,
-          locations,
-          operation,
-          store,
-        };
-      });
-    });
+const GET_USER_ID = gql`
+  {
+    currentUser {
+      id
+    }
   }
-  if (networkError) bugsnag.notify(networkError);
-});
+`;
+
+const bugsnagLink = onError(
+  async ({ graphQLErrors, networkError, operation }) => {
+    const { currentUser } = await client.readQuery({ query: GET_USER_ID });
+    if (graphQLErrors) {
+      graphQLErrors.forEach(({ message, locations, path }) => {
+        bugsnag.notify(new Error(message), (report) => {
+          if (operation.variables && operation.variables.password) {
+            // eslint-disable-next-line
+          delete operation.variables.password;
+          }
+          if (path) {
+            // eslint-disable-next-line
+            report.context = path.join('/');
+          }
+          // eslint-disable-next-line
+          report.metadata = {
+            path,
+            locations,
+            operation,
+            user: { id: currentUser.id },
+          };
+        });
+      });
+    }
+    if (networkError)
+      bugsnag.notify(networkError, (report) => {
+        // eslint-disable-next-line
+        report.metadata = { user: { id: currentUser.id } };
+      });
+  }
+);
 
 export { bugsnag as default, bugsnagLink };
