3c177de0f6cd9786d190bc494ab4c9e9646793ba
Michael Neeley <micneeley14@gmail.com>
2020-02-18 18:46:01 -0500

    wraps query safely

--- src/bugsnag.js
+++ src/bugsnag.js
@@ -9,44 +9,58 @@ configuration.apiKey = Config.BUGSNAG_API_KEY;
 configuration.releaseStage = Config.BUGSNAG_STAGE || 'development';
 const bugsnag = new Client(configuration);
 
-const GET_USER_ID = gql`
-  {
-    currentUser {
-      id
-    }
+const getUser = async () => {
+  let user;
+  try {
+    const {
+      currentUser: { id },
+    } = await client.readQuery({
+      query: gql`
+        {
+          currentUser {
+            id
+          }
+        }
+      `,
+    });
+    user = { id };
+  } catch (e) {
+    user = null;
   }
-`;
+  return user;
+};
 
-const bugsnagLink = onError(
-  async ({ graphQLErrors, networkError, operation }) => {
-    const { currentUser } = await client.readQuery({ query: GET_USER_ID });
-    if (graphQLErrors) {
-      graphQLErrors.forEach(({ message, locations, path }) => {
-        bugsnag.notify(new Error(message), (report) => {
-          if (operation.variables && operation.variables.password) {
-            // eslint-disable-next-line
-          delete operation.variables.password;
-          }
-          if (path) {
-            // eslint-disable-next-line
-            report.context = path.join('/');
-          }
+const bugsnagLink = onError(({ graphQLErrors, networkError, operation }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(async ({ message, locations, path }) => {
+      const user = await getUser();
+      bugsnag.notify(new Error(message), (report) => {
+        if (operation.variables && operation.variables.password) {
           // eslint-disable-next-line
+            delete operation.variables.password;
+        }
+        if (path) {
+          // eslint-disable-next-line
+            report.context = path.join('/');
+        }
+        // eslint-disable-next-line
           report.metadata = {
-            path,
-            locations,
-            operation,
-            user: { id: currentUser.id },
-          };
-        });
+          path,
+          locations,
+          operation,
+          user,
+        };
       });
-    }
-    if (networkError)
+    });
+  }
+  if (networkError)
+    (async () => {
+      const user = await getUser();
       bugsnag.notify(networkError, (report) => {
         // eslint-disable-next-line
-        report.metadata = { user: { id: currentUser.id } };
+        report.metadata = { user };
       });
-  }
-);
+    })();
+});
 
 export { bugsnag as default, bugsnagLink };
