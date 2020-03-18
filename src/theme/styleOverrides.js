6c79b070186e8971930c4916303892d9b116f538
richarddubay <richardtdubayjr@gmail.com>
2020-03-18 15:51:03 -0400

    Fixed overrides

--- src/theme/styleOverrides.js
+++ src/theme/styleOverrides.js
@@ -1,6 +1,5 @@
 /* Export your custom style overrides here. */
 export default {
-  'ui-kit.CardLabel': {
-    borderWidth: 0,
-  },
+  'ui-kit.CardLabel': { borderWidth: 0 },
+  'Button.Link': { fontWeight: '600' },
 };
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -113,11 +113,6 @@ export const buttons = ({ colors: themeColors, alpha: themeAlpha }) => ({
   },
 });
 
-export const overrides = {
-  'ui-kit.CardLabel': { borderWidth: 0 },
-  'Button.Link': { fontWeight: '600' },
-};
-
 export default {
   colors,
   typography,
