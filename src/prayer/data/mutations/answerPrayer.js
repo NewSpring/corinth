0607d7e1737c81ee2029fb05fbd4ed4a5262d525
richarddubay <richardtdubayjr@gmail.com>
2020-04-30 10:37:32 -0400

    Corrected the name of the mutation so that it actually works

--- src/prayer/data/mutations/answerPrayer.js
+++ src/prayer/data/mutations/answerPrayer.js
@@ -2,7 +2,7 @@ import gql from 'graphql-tag';
 
 export default gql`
   mutation AnswerPrayer($id: ID!, $answer: String!) {
-    addPrayer(id: $id, answer: $answer) {
+    answerPrayer(id: $id, answer: $answer) {
       id
       text
       answer
