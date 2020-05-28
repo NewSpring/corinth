5c990e66197d24cddc50334798a3fcd8be1c31d5
Michael Neeley <micneeley14@gmail.com>
2020-05-28 11:55:51 -0400

    its working

--- src/content-single/WeekendContentItem/SermonNotes/NoteComment/index.js
+++ src/content-single/WeekendContentItem/SermonNotes/NoteComment/index.js
@@ -5,18 +5,31 @@ import NoteComment from './NoteComment';
 import SAVE_NOTES_COMMENT from './saveNotesComment';
 
 const NoteCommentConnected = ({ contentID, noteID, onChange, initialText }) => {
-  const [text, setText] = useState(initialText);
-  const handleChange = (newText) => {
+  const [timer, setTimer] = useState(null);
+  const [textToSave, setText] = useState(initialText);
+  const handleChange = (saveComment) => (newText) => {
     setText(newText);
     onChange(newText);
+
+    // autosave after 10 sec
+    // check for existing timeout, clear it, and set a new one
+    clearTimeout(timer);
+    const newTimer = setTimeout(() => {
+      console.count('saving');
+      saveComment();
+    }, 10000);
+    setTimer(newTimer);
   };
   return (
     <Mutation
       mutation={SAVE_NOTES_COMMENT}
-      variables={{ contentID, noteID, text }}
+      variables={{ contentID, noteID, text: textToSave }}
     >
-      {(save) => (
-        <NoteComment onChange={handleChange} initialText={initialText} />
+      {(saveComment) => (
+        <NoteComment
+          onChange={handleChange(saveComment)}
+          initialText={initialText}
+        />
       )}
     </Mutation>
   );
