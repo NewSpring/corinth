9407b0f48798557777f22e55d3158f4b71ad98d8
Michael Neeley <micneeley14@gmail.com>
2020-05-28 11:31:14 -0400

    snap

--- src/content-single/WeekendContentItem/SermonNotes/NoteComment/index.js
+++ src/content-single/WeekendContentItem/SermonNotes/NoteComment/index.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Mutation } from 'react-apollo';
 import NoteComment from './NoteComment';
 import SAVE_NOTES_COMMENT from './saveNotesComment';
 
-const NoteCommentConnected = ({ contentID, noteID, ...props }) => (
-  <Mutation mutation={SAVE_NOTES_COMMENT} variables={{ contentID, noteID }}>
-    {(save) => <NoteComment {...props} />}
-  </Mutation>
-);
+const NoteCommentConnected = ({ contentID, noteID, onChange, initialText }) => {
+  const [text, setText] = useState(initialText);
+  const handleChange = (newText) => {
+    setText(newText);
+    onChange(newText);
+  };
+  return (
+    <Mutation
+      mutation={SAVE_NOTES_COMMENT}
+      variables={{ contentID, noteID, text }}
+    >
+      {(save) => (
+        <NoteComment onChange={handleChange} initialText={initialText} />
+      )}
+    </Mutation>
+  );
+};
 
 NoteCommentConnected.propTypes = {
   contentID: PropTypes.string,
   noteID: PropTypes.string,
+  onChange: PropTypes.func,
+  initialText: PropTypes.string,
 };
 
 export default NoteCommentConnected;
