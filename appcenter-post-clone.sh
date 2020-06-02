dc6e183ae209d447228a2e4c0b794156a19513db
Michael Neeley <micneeley14@gmail.com>
2020-06-02 14:23:52 -0400

    chore: Upload Sourcemaps to Bugsnag

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -21,3 +21,44 @@ COCOAPODS_VER=$(sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' ios/Podfile.lock)
 
 echo "Installing CocoaPods version $COCOAPODS_VER"
 sudo gem install cocoapods -v "$COCOAPODS_VER"
+
+# upload Bugsnag source maps
+VERSION=$(sed -n -E "/\"version\"/s/.* \"(.*)\"\,/\1/p" package.json | sed -n 1p)
+VERSION_CODE=$(sed -n "/versionCode/s/.* //p" android/app/build.gradle | sed -n 1p)
+BUNDLE_VERSION=$(sed -n -E "/<string>[0-9]+<\/string>/s/<string>(.*)<\/string>/\1/p" ios/newspringchurchapp/Info.plist | sed -n 1p)
+
+# generate and upload for ios
+yarn react-native bundle \
+    --platform ios \
+    --dev false \
+    --entry-file index.js \
+    --bundle-output ios-release.bundle \
+    --sourcemap-output ios-release.bundle.map
+
+curl --http1.1 https://upload.bugsnag.com/react-native-source-map \
+   -F apiKey="$BUGSNAG_API_KEY" \
+   -F appVersion="$VERSION" \
+   -F appBundleVersion="$BUNDLE_VERSION" \
+   -F dev=false \
+   -F platform=ios \
+   -F sourceMap=@ios-release.bundle.map \
+   -F bundle=@ios-release.bundle \
+   -F projectRoot="$(pwd)"
+
+# generate and upload for android
+yarn react-native bundle \
+    --platform android \
+    --dev false \
+    --entry-file index.js \
+    --bundle-output android-release.bundle \
+    --sourcemap-output android-release.bundle.map
+
+curl --http1.1 https://upload.bugsnag.com/react-native-source-map \
+   -F apiKey="$BUGSNAG_API_KEY" \
+   -F appVersion="$VERSION" \
+   -F appVersionCode="$VERSION_CODE" \
+   -F dev=false \
+   -F platform=android \
+   -F sourceMap=@android-release.bundle.map \
+   -F bundle=@android-release.bundle \
+   -F projectRoot="$(pwd)"
--- scripts/bump-version-code.sh
+++ scripts/bump-version-code.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-# overwrites android version code
-VERSION_CODE=$(sed -n -e "/versionCode/s/.* //p" android/app/build.gradle | sed -n 1p)
+# overwrites version code
 # YYYYDDDmmm - year, day (out of 365), minute of the day (% of 1440)
 # 2020001500 - Jan 1, 2020 at 12pm
 MIN=$(bc <<< "scale=3; ($(date -u +%H) * 60 + $(date -u +%M)) / 1440")
-sed -i.bak -E "s/versionCode [0-9]+/versionCode $(date -u +"%Y%j")${MIN//.}/g" android/app/build.gradle
+CODE=$(date -u +"%Y%j")${MIN//.}
+sed -i.bak -E "s/versionCode [0-9]+/versionCode $CODE/g" android/app/build.gradle
+sed -i.bak -E "s/<string>[0-9]+<\/string>/<string>$CODE<\/string>/g" ios/newspringchurchapp/Info.plist
