0b50f422e348795674ef0bb3cdee1e6f91d13cd1
richarddubay <richardtdubayjr@gmail.com>
2020-07-22 11:50:22 -0400

    Fix linting

--- src/prayer/PrayerList/index.js
+++ src/prayer/PrayerList/index.js
@@ -3,7 +3,6 @@ import { View, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
 import Emoji from 'react-native-emoji';
 import { Query, Mutation } from 'react-apollo';
-import PropTypes from 'prop-types';
 
 import {
   BodyText,
