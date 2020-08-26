598fdea9da62abdcafbb8ce7c72db27e9f81311c
richarddubay <richardtdubayjr@gmail.com>
2020-08-26 12:02:51 -0400

    Fixed landing screen

--- src/LandingScreen.js
+++ src/LandingScreen.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { styled, ConnectedImage } from '@apollosproject/ui-kit';
+import { Image, StyleSheet } from 'react-native';
+import { styled } from '@apollosproject/ui-kit';
 
 import ApollosLandingScreen from './ui/LandingScreen';
 
 const FullScreenImage = styled({
   resizeMode: 'cover',
   ...StyleSheet.absoluteFill,
-})(ConnectedImage);
+  width: '100%',
+  height: '100%',
+})(Image);
 
 const LandingScreen = ({ navigation }) => (
   <ApollosLandingScreen
