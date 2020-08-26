443671a76429951af3db9c79d17c7dfd6ee2c5af
richarddubay <richardtdubayjr@gmail.com>
2020-08-26 12:51:02 -0400

    Corrected navigation on auth

--- src/Providers.js
+++ src/Providers.js
@@ -25,6 +25,7 @@ const AppProviders = (props) => (
         closeAuth={() =>
           checkOnboardingStatusAndNavigate({
             client,
+            navigation: NavigationService,
           })
         }
       >
