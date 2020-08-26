a44cf87a378177919c8fcb69a1ab39adf06d6b59
richarddubay <richardtdubayjr@gmail.com>
2020-08-26 12:51:22 -0400

    Fixed the button link style override

--- src/theme/styleOverrides.js
+++ src/theme/styleOverrides.js
@@ -1,5 +1,5 @@
 /* Export your custom style overrides here. */
 export default {
   'ui-kit.CardLabel': { borderWidth: 0 },
-  'Button.Link': { fontWeight: '600' },
+  'ui-kit.ButtonLink.ButtonLink': { fontWeight: '600' },
 };
