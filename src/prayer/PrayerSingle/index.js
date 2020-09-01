0b8f83dc7eb0d71f3b4d753578ddad6ba9e849e7
richarddubay <richardtdubayjr@gmail.com>
2020-09-01 11:28:13 -0400

    PrayerSingle now supports rendering from a notification

--- src/prayer/PrayerSingle/index.js
+++ src/prayer/PrayerSingle/index.js
@@ -1,6 +1,7 @@
 import React, { memo } from 'react';
 import { View, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
+import { Query } from 'react-apollo';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import {
@@ -12,9 +13,12 @@ import {
   ModalView,
   styled,
   Touchable,
+  ActivityIndicator,
+  ErrorCard,
 } from '@apollosproject/ui-kit';
 import { AnalyticsConsumer } from '@apollosproject/ui-analytics';
 import PrayerHeader from '../PrayerHeader';
+import GET_PRAYER from '../data/queries/getPrayer';
 
 const GreyH5 = styled(({ theme }) => ({
   color: theme.colors.text.tertiary,
@@ -45,24 +49,55 @@ const ScrollArea = styled(({ theme }) => ({
 const getNotification = (navigation) =>
   navigation ? navigation.getParam('notification', false) : false;
 
-const Wrapper = ({ navigation, ...props }) => {
-  const fromNotification = getNotification(navigation);
-  return fromNotification ? (
-    <ModalView onClose={() => navigation.popToTop()}>
-      <FlexedSafeAreaView>
-        <ScrollArea>
-          <ScrollView>{props.children}</ScrollView>
-        </ScrollArea>
-      </FlexedSafeAreaView>
-    </ModalView>
-  ) : (
-    <View>{props.children}</View>
-  );
-};
+const getPrayerId = (navigation) =>
+  navigation ? navigation.getParam('id', null) : null;
 
-Wrapper.propTypes = {
-  children: PropTypes.any, //eslint-disable-line
-};
+const PrayerFromNotification = ({ navigation }) => (
+  <ModalView onClose={() => navigation.popToTop()}>
+    <FlexedSafeAreaView>
+      <Query
+        query={GET_PRAYER}
+        variables={{ id: getPrayerId(navigation) }}
+        fetchPolicy={'cache-and-network'}
+      >
+        {({ data, loading, error }) => {
+          if (loading) return <ActivityIndicator />;
+          if (error) return <ErrorCard />;
+          const { prayer } = data;
+          return (
+            <ScrollArea>
+              <ScrollView>
+                <PrayerHeader
+                  avatarSize={'medium'}
+                  avatarSource={
+                    prayer.isAnonymous ? null : prayer.requestor.photo
+                  }
+                  title={`Pray for ${
+                    prayer.isAnonymous
+                      ? 'Request'
+                      : prayer.requestor.nickName || prayer.requestor.firstName
+                  }`}
+                  source={
+                    prayer.campus.name !== 'Web' ? prayer.campus.name : null
+                  }
+                />
+                <PrayerView>
+                  <BodyText>{prayer.text}</BodyText>
+                </PrayerView>
+                {prayer.answer && prayer.answer.length > 0 ? (
+                  <PrayerView>
+                    <H4>Answer:</H4>
+                    <BodyText>{prayer.answer}</BodyText>
+                  </PrayerView>
+                ) : null}
+              </ScrollView>
+            </ScrollArea>
+          );
+        }}
+      </Query>
+    </FlexedSafeAreaView>
+  </ModalView>
+);
 
 const PrayerSingle = memo(
   ({
@@ -74,63 +109,69 @@ const PrayerSingle = memo(
     action,
     isLoading,
     navigation,
-    ...props
   }) => (
-    <Wrapper navigation={navigation}>
-      {!isLoading && !getNotification(navigation) ? (
-        <AbsolutePositionedView>{action}</AbsolutePositionedView>
-      ) : null}
-      {showDate ? (
-        <GreyH5>
-          {prayer.startTime ? moment(prayer.startTime).fromNow() : ''}
-        </GreyH5>
-      ) : null}
-      {showHeader ? (
-        <PrayerHeader
-          avatarSize={avatarSize}
-          avatarSource={prayer.isAnonymous ? null : prayer.requestor.photo}
-          title={`Pray for ${
-            prayer.isAnonymous
-              ? 'Request'
-              : prayer.requestor.nickName || prayer.requestor.firstName
-          }`}
-          source={prayer.campus.name !== 'Web' ? prayer.campus.name : null}
-        />
-      ) : null}
-      <PrayerView>
-        <BodyText>{prayer.text}</BodyText>
-      </PrayerView>
-      {prayer.answer && prayer.answer.length > 0 ? (
-        <PrayerView>
-          <H4>Answer:</H4>
-          <BodyText>{prayer.answer}</BodyText>
-        </PrayerView>
-      ) : null}
-      {showHelp && !getNotification(navigation) ? (
-        <AnalyticsConsumer>
-          {({ track }) => (
-            <Touchable
-              onPress={() => {
-                props.navigation.navigate('ContentSingle', {
-                  // TODO: this should come from a content channel
-                  itemId: 'MediaContentItem:20f5b6548d64b1ac62a1c4b0deb0bfcb',
-                  itemTitle: 'Learning how to pray like Jesus',
-                  isolated: true,
-                });
-                track({ eventName: "Clicked 'How to Pray'" });
-              }}
-            >
-              <View>
-                <ChannelLabel
-                  icon="information"
-                  label="Not sure how to pray? Read this."
-                />
-              </View>
-            </Touchable>
-          )}
-        </AnalyticsConsumer>
-      ) : null}
-    </Wrapper>
+    <>
+      {getNotification(navigation) ? (
+        <PrayerFromNotification navigation={navigation} />
+      ) : (
+        <View>
+          {!isLoading && !getNotification(navigation) ? (
+            <AbsolutePositionedView>{action}</AbsolutePositionedView>
+          ) : null}
+          {showDate ? (
+            <GreyH5>
+              {prayer.startTime ? moment(prayer.startTime).fromNow() : ''}
+            </GreyH5>
+          ) : null}
+          {showHeader ? (
+            <PrayerHeader
+              avatarSize={avatarSize}
+              avatarSource={prayer.isAnonymous ? null : prayer.requestor.photo}
+              title={`Pray for ${
+                prayer.isAnonymous
+                  ? 'Request'
+                  : prayer.requestor.nickName || prayer.requestor.firstName
+              }`}
+              source={prayer.campus.name !== 'Web' ? prayer.campus.name : null}
+            />
+          ) : null}
+          <PrayerView>
+            <BodyText>{prayer.text}</BodyText>
+          </PrayerView>
+          {prayer.answer && prayer.answer.length > 0 ? (
+            <PrayerView>
+              <H4>Answer:</H4>
+              <BodyText>{prayer.answer}</BodyText>
+            </PrayerView>
+          ) : null}
+          {showHelp && !getNotification(navigation) ? (
+            <AnalyticsConsumer>
+              {({ track }) => (
+                <Touchable
+                  onPress={() => {
+                    navigation.navigate('ContentSingle', {
+                      // TODO: this should come from a content channel
+                      itemId:
+                        'MediaContentItem:20f5b6548d64b1ac62a1c4b0deb0bfcb',
+                      itemTitle: 'Learning how to pray like Jesus',
+                      isolated: true,
+                    });
+                    track({ eventName: "Clicked 'How to Pray'" });
+                  }}
+                >
+                  <View>
+                    <ChannelLabel
+                      icon="information"
+                      label="Not sure how to pray? Read this."
+                    />
+                  </View>
+                </Touchable>
+              )}
+            </AnalyticsConsumer>
+          ) : null}
+        </View>
+      )}
+    </>
   )
 );
 
