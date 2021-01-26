8c6051eef6bf007968f186ce020a6ab1643810eb
Vincent Wilson <vince@classyh.at>
2021-01-26 10:03:50 -0500

    iOS Booting

--- index.js
+++ index.js
@@ -3,20 +3,7 @@ import { AppRegistry, YellowBox } from 'react-native';
 
 import ApollosConfig from '@apollosproject/config';
 
-// temp fix for the promise.finally
-// https://github.com/storybookjs/storybook/issues/8371
-const fn = Promise.prototype.finally;
-const Storybook = require('./storybook').default; // eslint-disable-line
-Promise.prototype.finally = fn; // eslint-disable-line
-const useStorybook = ApollosConfig.STORYBOOK === 'true';
-
-const MainApp = require('./src').default;
-
-let App = MainApp;
-if (useStorybook) {
-  App = Storybook;
-}
-
+const App = require('./src').default;
 YellowBox.ignoreWarnings([
   'Warning: isMounted(...) is deprecated',
   'Module RCTImageLoader',
