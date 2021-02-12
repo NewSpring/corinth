9957dd02d4a45e68def47cc5a3a1c3304c80a2a9
Vincent Wilson <vince@classyh.at>
2021-02-12 15:10:03 -0500

    Update to canary

--- loadConfig.js
+++ loadConfig.js
@@ -2,6 +2,28 @@ import ApollosConfig from '@apollosproject/config';
 import FRAGMENTS from '@apollosproject/ui-fragments';
 import gql from 'graphql-tag';
 
+// import fragmentTypes from './src/client/fragmentTypes.json';
+// Create a map all the interfaces each type implements.
+// If UniversalContentItem implements Node, Card, and ContentNode,
+// our typemap would be { UniversalContentItem: ['Node', 'Card', 'ContentNode'] }
+// const TYPEMAP = fragmentTypes.__schema.types.reduce((acc, curr) => {
+//   const { name } = curr;
+//   const types = Object.fromEntries(
+//     curr.possibleTypes.map((type) => [type.name, name])
+//   );
+//   Object.keys(types).forEach((key) => {
+//     acc[key] = acc[key] ? [...acc[key], types[key]] : [types[key]];
+//   });
+//   return acc;
+// }, {});
+
+// Hardcode the typemap for now.
+// This changes what connected components are displayed as loading states on content items
+const TYPEMAP = {
+  ContentSeriesContentItem: ['ContentParentNode'],
+  DevotionalContentItem: ['ContentChildNode'],
+};
+
 const liteFeatures = `
       fragment LiteFeaturesFragment on Feature {
         id
@@ -60,6 +82,7 @@ const liteFeatures = `
     `;
 
 ApollosConfig.loadJs({
+  TYPEMAP,
   FRAGMENTS: {
     ...FRAGMENTS,
     // Same as core, but not PrayerListFeature.
