a0bd84284f9101c0458860186006e9162b460914
Vincent Wilson <vince@classyh.at>
2021-03-23 09:24:15 -0400

    Fix android hermes issue

--- android/app/src/main/java/com/newspringchurchapp/MainApplication.java
+++ android/app/src/main/java/com/newspringchurchapp/MainApplication.java
@@ -1,5 +1,8 @@
 package com.newspringchurchapp;
 
+import com.facebook.react.bridge.JSIModulePackage;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
+
 import android.app.Application;
 import android.content.Context;
 import android.util.Log;
@@ -40,6 +43,11 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected String getJSMainModuleName() {
       return "index";
+    }
+
+    @Override
+    protected JSIModulePackage getJSIModulePackage() {
+      return new ReanimatedJSIModulePackage(); // <- add
     }
   };
 
