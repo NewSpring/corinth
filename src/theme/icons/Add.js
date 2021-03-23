716fe297fc24ee449463b8dc5e185ba3882a0b9c
Michael Neeley <micneeley14@gmail.com>
2021-03-23 15:23:14 -0400

    linter

--- src/theme/icons/Add.js
+++ src/theme/icons/Add.js
@@ -4,7 +4,13 @@ import Svg, { Path } from 'react-native-svg';
 import { makeIcon } from '@apollosproject/ui-kit';
 
 const Icon = makeIcon(({ size, fill, ...otherProps } = {}) => (
-  <Svg width={size} fill={fill} height={size} viewBox="0 0 24 24" {...otherProps}>
+  <Svg
+    width={size}
+    fill={fill}
+    height={size}
+    viewBox="0 0 24 24"
+    {...otherProps}
+  >
     <Path
       d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z"
       // fill={'none'}
