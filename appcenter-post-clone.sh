ae623aa3891fd85977b985e173fc1a362df24078
Michael Neeley <micneeley14@gmail.com>
2021-12-17 12:06:00 -0500

    attempt to get keystore stuff from appcenter

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+env
+find ./ -name "*.keystore"
+
 # Swaps out all placeholder env variables w/ their real values
 # Placeholders look like "$ONE_SIGNAL_KEY"
 grep -o '\$.*' .env.production | sed 's/\$\(.*\)/\1/' | xargs -I {} sh -c "sed -i -e 's/\$"{}"/'$"{}"'/' .env.production"
 # Make sure ReactNativeConfig picks up values from prod env file.
 cp .env.production .env
-
 
 # set AppCenter secret
 sed -i "" -E "s/fake-secret-123/$APPCENTER_SECRET/g" ios/AppCenter-Config.plist
