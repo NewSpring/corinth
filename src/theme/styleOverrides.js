4c8f2b94500ef87a89e813ebe18a34724e0890cd
Conrad VanLandingham <conrad@differential.com>
2022-01-04 14:22:07 -0600

    increase `ScriptureText` size

--- src/theme/styleOverrides.js
+++ src/theme/styleOverrides.js
@@ -1,4 +1,8 @@
 /* Export your custom style overrides here. */
 export default {
   'ui-kit.ButtonLink.ButtonLink': { fontWeight: '600' },
+  ScriptureText: (theme) => ({
+    fontSize: theme.helpers.rem(1.25),
+    lineHeight: theme.helpers.verticalRhythm(1.5, 1.625),
+  }),
 };
